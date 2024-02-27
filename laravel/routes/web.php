03324da23b0109bc9071c4f777dd3a94be6ebcf5
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2024-02-27 12:12:24 +0100

    ruta index y login

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,14 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/index', function () {
+        return view('index');
+    })->name('index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
