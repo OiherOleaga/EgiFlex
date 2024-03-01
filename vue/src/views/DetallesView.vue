4ea932e1b38818594d2d94f6c37c4a86a44cdfd6
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-03-01 08:58:42 +0100

    series

--- vue/src/views/DetallesView.vue
+++ vue/src/views/DetallesView.vue
@@ -7,6 +7,7 @@ let args = window.location.search.split("?")[1].split("=")
 if (args[0] == 's') {
     POST("/getDetallesSerie", { id: args[1] }).then(res => {
         detalles.value = res.detalles  
+        console.log(res.detalles)
     })
 } else if (args[0] == 'p') {
     POST("/getDetallesPelicula", { id: args[1] }).then(res => {
@@ -14,11 +15,18 @@ if (args[0] == 's') {
     })
 }
 
-POST("/get");
-
 </script>
 
 <template>
-    <h1>detalles</h1>
-
+    <article class="w-100 min-vh-100">
+        <div class="content">
+            <div v-if="detalles" class="row .text-light">
+                <p class="col-12">Titulo: {{ detalles.titulo }}</p>
+                <p class="col-12">Director: {{ detalles.director }}</p>
+                <p class="col-12">: {{ detalles.ano_lanzamiento }}</p>
+                <p class="col-12">Director: {{ detalles.director }}</p>
+                <p class="col-12">Director: {{ detalles.director }}</p>
+            </div>
+        </div>
+    </article>
 </template>
